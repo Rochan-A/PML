6b63303df0937987622edaa4db44b26567dddc5d
Rochan Avlur <rochan.avlur@gmail.com>
2022-12-06 13:10:38 -0600

    Fixed swingup env v0.62

--- envs/term_rew.py
+++ envs/term_rew.py
@@ -2,32 +2,33 @@ import torch, math
 import numpy as np
 
 def cartpole_upright_term(action, state):
-    done =(state[:, 0] < -2.4) \
-        * (state[:, 0] > 2.4) \
-        * (state[:, 2] < -(12 * 2 * math.pi / 360)) \
-        * (state[:, 2] > (12 * 2 * math.pi / 360))
+    done = (state[:, 0] < -2.4) \
+        or (state[:, 0] > 2.4) \
+        or (state[:, 2] < -(12 * 2 * math.pi / 360)) \
+        or (state[:, 2] > (12 * 2 * math.pi / 360))
     return done
 
 def cartpole_upright_reward(action, next_state):
     ones = torch.ones((next_state.shape[0]), device=next_state.device)
     zeros = torch.zeros((next_state.shape[0]), device=next_state.device)
     done = (next_state[:, 0] < -2.4) \
-        * (next_state[:, 0] > 2.4) \
-        * (next_state[:, 2] < -(12 * 2 * math.pi / 360)) \
-        * (next_state[:, 2] > (12 * 2 * math.pi / 360))
+        or (next_state[:, 0] > 2.4) \
+        or (next_state[:, 2] < -(12 * 2 * math.pi / 360)) \
+        or (next_state[:, 2] > (12 * 2 * math.pi / 360))
     k = torch.where(done, zeros, ones).reshape(-1, 1)
     return k
 
 def cartpole_swingup_term(action, state):
-    done = (state[:, 0] < -2.4) * (state[:, 0] > 2.4)
+    done = (state[:, 0] < -2.4) or (state[:, 0] > 2.4)
     return done
 
 class cartpole_swingup_rew:
     def __init__(self, l) -> None:
-        self.l
+        self.l = l
 
     def __call__(self, action, next_state):
-        x, theta, = next_state[:, 0], next_state[:, 2]
+        x, cos_theta, sin_theta = next_state[:, 0], next_state[:, 2], next_state[:, 3]
+        theta = 2*torch.arctan(sin_theta/(1 + cos_theta))
         length = self.l  # pole length
         x_tip_error = x - length * np.sin(theta)
         y_tip_error = length - length * np.cos(theta)
